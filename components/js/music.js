eabc117b34d7b8fae81ceaa15c2a7c045f2bd5fd
Alice Le <lealice@gmail.com>
2019-03-19 10:22:48 -0700

    Started music obj and changed html title

---
+++ components/js/music.js
@@ -0,0 +1,13 @@
+class Music{
+    constructor(){
+        this.ajaxObject = {
+            dataType: "json",
+            url: "https://rss.itunes.apple.com/api/v1/us/itunes-music/top-songs/all/25/explicit.json",
+            method: "GET",
+            success: 
+            // error: 
+            }
+
+        }
+    }
+}
