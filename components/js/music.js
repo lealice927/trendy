1a2b79d8177a187ba7bae872d8bae12ae7d56ad0
Alice Le <lealice@gmail.com>
2019-03-19 16:34:38 -0700

    Completed loop through array of top 25 songs, everything displays to DOM

--- components/js/music.js
+++ components/js/music.js
@@ -17,9 +17,33 @@ class Music {
             method: "GET",
             success: (response) => {
                 console.log(response);
-            }
+                // debugger;
+                for (var index = 0; index <= response.feed.results.length; index++) {
+                var albumImage = response.feed.results[index].artworkUrl100;
+                var albumName = response.feed.results[index].collectionName;
+                var artist = response.feed.results[index].artistName;
+                var songName = response.feed.results[index].name;
+                var iTunesAlbum = response.feed.results[index].url;
+                var newDiv = iTunesMusic.newElement(albumImage, albumName, artist, songName, iTunesAlbum);
+                iTunesMusic.render('#main-content', newDiv)
+                }
+            },
             // error: 
         }
         $.ajax(ajaxObject);
+    }
+    newElement(image, album, artist, song, link) {
+        const imageBox = $('<div>').addClass('image-box').css('background-image', `url(${image})`);
+        const albumLink = $('<a>').attr('href', link).text(album);
+        const albumTitle = $('<div>').addClass('title-box').append(albumLink);
+        const artistDescription = $('<p>').text('Artist: ' + artist);
+        const songName = $('<p>').text('Song Name: ' + song);
+        const artistAndSongDescription = $('<div>').addClass('description-box').append(artistDescription, songName);
+        const textBox = $('<div>').addClass('text-box').append(albumTitle, artistAndSongDescription);
+        const contentBox = $('<div>').addClass('content-box').append(imageBox, textBox);
+        return contentBox;
+    }
+    render(divContainer, newElement) {
+        $(divContainer).append(newElement);
     }
 }
