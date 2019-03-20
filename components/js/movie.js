96e68848c9309b106f10e8452922fc803527318b
Jimmy Pang <cpangwork@gmail.com>
2019-03-19 17:27:04 -0700

    530pm

--- components/js/movie.js
+++ components/js/movie.js
@@ -3,12 +3,18 @@ $(document).ready(initializeApp);
 var startQuotes = null;
 function initializeApp(){
     startQuotes = new Quotes(); 
+    startQuotes.differentResult();
 }
 
 
 class Quotes{
     constructor(){
-        this.getDataFromServer();       //static function inside constructor to trigger main data receiving function
+    this.pictures = [];
+    }
+    differentResult(){
+        for (var index = 0; index < 4; index++){
+        this.getDataFromServer();  
+      }
     }
     getDataFromServer(){        //data receiving function
         const ajaxObject = {        //ajax result container
@@ -16,25 +22,32 @@ class Quotes{
             url: 'https://api.whatdoestrumpthink.com/api/v1/quotes/random',
             method: 'GET',
             success: (response)=>{
-                for (let index = 0; index<10; index++){     //creates for loop for automatic div creation
-                    const message = response.message;       //extract message from object
-                    const newTxtDiv = startQuotes.dom(message);      //inputting message from txtDom into startQuotes object 
+                    let message = response.message;       //extract message from object
+                    let quotedMessage = '"' + message + '"';
+                    const newTxtDiv = startQuotes.dom(quotedMessage);      //inputting message from txtDom into startQuotes object 
                     startQuotes.render("#main-content", newTxtDiv);      //targeting description-box and put message content into render to DOM
-                }
                 
             },
             error: ()=>alert('Failed to contact server')
         }
         $.ajax(ajaxObject);
     }
-        dom(messagePassedIn){        //function returns message from URL
-            const descriptionBox = $('<div>').addClass('quoteBox').append(messagePassedIn);     //creates container for message from URL to be held
-            const txtBox = $('<div>').addClass('textBox').append(descriptionBox);  //creates container for message from URL to be held
-            const contentBox = $('<div>').addClass('contentBox').append(txtBox); 
-            return contentBox;  
+    dom(messagePassedIn){        //function returns message from URL
+        const descriptionBox = $('<div>').addClass('quoteBox').append(messagePassedIn);     //creates container for message from URL to be held
+        const txtBox = $('<div>').addClass('textBox').append(descriptionBox);  //creates container for message from URL to be held
+        const contentBox = $('<div>').addClass('contentBox').append(txtBox); 
+        return contentBox;  
+    }
+
+    randomPicture(result){
+        var result = null;
+        for (pictures = 0; pictures < this.pictures.length; pictures++){
+            if (Math.floor(Math.random(result) * 9)){
+                return result;
+            }
         }
-        render(divContainer, newElement){
-            $(divContainer).append(newElement);
-        
+    }
+    render(divContainer, newElement){
+        $(divContainer).append(newElement);
     }
 }
