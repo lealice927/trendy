db3688cac395a7e4459938a0989a78d3220e51c5
Kylie Lin <kylieclin@gmail.com>
2019-03-19 23:00:47 -0700

    add food content class

--- components/js/main.js
+++ components/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(startApp);
 function startApp(){
 
     let food = new Food();
-    // food.getDataFromServer();
     food.generateSearchData(); 
     food.getCurrentLocation();
+    food.addEventListener();
 }
