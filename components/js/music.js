f4344a716bad82b2e2cabff895504585c8410d07
Alice Le <lealice@gmail.com>
2019-03-20 14:37:07 -0700

    created landing.js page created new Class to make landing page dynamic, will render to DOM

--- components/js/music.js
+++ components/js/music.js
@@ -18,13 +18,13 @@ class Music {
             success: (response) => {
                 console.log(response);
                 // debugger;
-                for (var index = 0; index <= 10; index++) {
-                var albumImage = response.feed.results[index].artworkUrl100;
-                var albumName = `# ${index+1} :  ${response.feed.results[index].collectionName}`;
-                var artist = response.feed.results[index].artistName;
-                var songName = response.feed.results[index].name;
-                var iTunesAlbum = response.feed.results[index].url;
-                var newDiv = iTunesMusic.newElement(albumImage, albumName, artist, songName, iTunesAlbum);
+                for (let index = 0; index < 10; index++) {
+                const albumImage = response.feed.results[index].artworkUrl100;
+                const albumName = `# ${index+1} :  ${response.feed.results[index].collectionName}`;
+                const artist = response.feed.results[index].artistName;
+                const songName = response.feed.results[index].name;
+                const iTunesAlbum = response.feed.results[index].url;
+                const newDiv = iTunesMusic.newElement(albumImage, albumName, artist, songName, iTunesAlbum);
                 iTunesMusic.render('#main-content', newDiv)
                 }
             },
@@ -35,7 +35,7 @@ class Music {
     newElement(image, album, artist, song, link) {
         const imageBox = $('<div>').addClass('image-box').css('background-image', `url(${image})`);
         const albumText = $('<p>').text(album);
-        const albumLink = $('<a>').attr('href', link);
+        const albumLink = $('<a target="_blank">').attr('href', link);
         albumLink.append(albumText);
         const albumTitle = $('<div>').addClass('title-box').append(albumLink);
         const artistDescription = $('<p>').text('Artist: ' + artist);
@@ -49,3 +49,4 @@ class Music {
         $(divContainer).append(newElement);
     }
 }
+
